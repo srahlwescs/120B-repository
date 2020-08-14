b2f488ef8a5c55e34e03ca570b6247064a67760b
Shawn Rahlwes <srahl001@ucr.edu>
2020-08-13 17:53:22 -0700

    A new version of the garage file

--- main.c
+++ main.c
@@ -1,28 +1,44 @@
-int const PA0=11;
-int const PA1=12;
-void setup()
-{
-pinMode(PA1, INPUT);
-pinMode(PA0,INPUT);
-pinMode(PB0,OUTPUT);
-}
+#include <avr/io.h>
+#ifdef _SIMULATE
+#include simAVRHeader.h
+#endif
 
-void loop()
-{
-if(PA0==0 && PA1==0)
-{
-digitalWrite(PB0,LOW);
-}
-else if( PA0==1 && PA1==0)
-{
-digitalWrite(PB0,HIGH);
-}
-else if(PA0==0 && PA1==1)
-{
-digitalWrite(PB0,LOW);
-}
-else
-{
-digitalWrite(PB0,LOW);
-}
+int main(void){
+
+
+
+        DDRA = 0x00;
+        PORTA = 0x02;// 0000 0001 means door is open
+        // 0000 0010 means light is sensed
+        DDRB = 0xFF;    // Configure port B's 8 pins as outputs.
+        PORTB = 0x00;
+        inputB0 = PINB & 0x01;
+        PA0 = dooropen;
+        PA1 = lightsensed;
+        PB0 = LEDilluminated;
+
+        unsigned char doorsensor = 0;
+        unsigned char lightsensor = 0;
+        unsigned char led = 0;
+
+        while(1){
+
+        if (doorsensor = 2 && lightsensor = 1){
+                led = 1;
+        }
+                else{
+                led = 0;
+        }
+
+
+
+
+
+        PORTB = led;
+        }
+
+return 1;
+
+
+
 }
